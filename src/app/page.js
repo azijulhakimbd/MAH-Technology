8d0ad232c5534e3b3d4f0fb9dc8d1a409737f08f
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2025-09-27 16:32:32 +0600

    [Added]:About Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 
 import Image from "next/image";
 import Hero from "./Components/Hero";
+import AboutSection from "./Components/AboutSection";
 
 export default function Home() {
   return (
     <div>
      {/* Hero Spotlight */}
      <Hero/>
+     {/* About Section */}
+     <AboutSection />
     </div>
  
   );
